2244d46f8a8328dcc604da8b3f557c5b39699286
Liriali4 <lirialia51@gmail.com>
2026-01-01 20:14:10 +0100

    Feat: Add folder struture

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,14 +4,11 @@ import { StatusCodes } from "http-status-codes";
 const router = Router();
 
 
-router.get("/teste", (_req, res) => {
+router.get("/", (_req, res) => {
     res.send("API rodando 🚀: Método GET");
 });
 
-
 router.post("/teste", (req, res) => {
-   // console.log(req.query.teste);
-    console.log(req.body);    
    // res.send("API rodando 🚀: Método POST");
     return res.status(StatusCodes.ACCEPTED).json(req.body);
 });
