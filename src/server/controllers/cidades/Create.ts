38a9d027d9a1e386010416c1070fceda01d966d8
Liriali4 <lirialia51@gmail.com>
2026-05-13 15:37:22 +0100

    feat: Validação com o yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,28 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
 
 interface ICidade {
     nome: string;
 }
 
-export const create = (req: Request<ICidade>, res: Response) => {
-    const data: ICidade = req.body;
-    console.log(data);
+const bodyvalidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3).max(50),
+});
+
+export const create = async (req: Request<ICidade>, res: Response) => {
+    let validateData: ICidade | undefined = undefined;
+    try {
+        validateData = await bodyvalidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors: {
+                default: yupError.errors,
+            }
+        })
+    }
+
+    console.log(validateData);
 
     return res.send('create cidade');
 }
